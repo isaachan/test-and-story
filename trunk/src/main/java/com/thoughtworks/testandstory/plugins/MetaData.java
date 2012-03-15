806ba1930e8e28a434cb1111a832ce4206264c8b
isaachanstar@gmail.com <isaachanstar@gmail.com@c7dc0373-66dd-5fc0-b770-3954006a1860>
2012-03-15 11:29:25 +0000

    add missing MetaData.java

---
+++ trunk/src/main/java/com/thoughtworks/testandstory/plugins/MetaData.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.testandstory.plugins;
+
+import java.lang.annotation.Annotation;
+
+import javassist.ClassPath;
+import javassist.ClassPool;
+import javassist.NotFoundException;
+import javassist.bytecode.AnnotationsAttribute;
+import javassist.bytecode.annotation.NoSuchClassError;
+
+public class MetaData {
+	
+	protected AnnotationsAttribute attribute;
+
+	@SuppressWarnings("unchecked")
+	private <A> A getAnnotationBy(Class<A> annotationClass)	throws ClassNotFoundException, NoSuchClassError {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		ClassPool classPool = ClassPool.getDefault();
+		return (A) attribute.getAnnotation(annotationClass.getName()).toAnnotationType(classLoader, classPool);
+	}
+	
+	public boolean isAnnotationPresent(Class<? extends Annotation> annotationClass) {
+		if (attribute == null) return false;
+		return null != attribute.getAnnotation(annotationClass.getName());
+	}
+	
+	public <A extends Annotation> A getAnnotation(Class<A> annotationClass) {
+		try {
+			return getAnnotationBy(annotationClass);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
