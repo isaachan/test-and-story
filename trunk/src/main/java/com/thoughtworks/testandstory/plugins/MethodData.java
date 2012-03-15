ddce37624cdb18f13e88b58ee475035f9f827d2e
isaachanstar@gmail.com <isaachanstar@gmail.com@c7dc0373-66dd-5fc0-b770-3954006a1860>
2012-03-15 10:16:19 +0000

    intruduce MetaData

--- trunk/src/main/java/com/thoughtworks/testandstory/plugins/MethodData.java
+++ trunk/src/main/java/com/thoughtworks/testandstory/plugins/MethodData.java
@@ -1,16 +1,11 @@
 package com.thoughtworks.testandstory.plugins;
 
-import java.lang.annotation.Annotation;
-
-import javassist.ClassPool;
 import javassist.bytecode.AnnotationsAttribute;
 import javassist.bytecode.MethodInfo;
-import javassist.bytecode.annotation.NoSuchClassError;
 
-class MethodData {
+class MethodData extends MetaData {
 
 	private MethodInfo methodInfo;
-	private AnnotationsAttribute attribute;
 
 	protected MethodData() {}
 	
@@ -19,29 +14,9 @@ class MethodData {
 		this.attribute = (AnnotationsAttribute) methodInfo.getAttribute(AnnotationsAttribute.visibleTag);
 	}
 	
-	public <A extends Annotation> A getAnnotation(Class<A> annotationClass) {
-		try {
-			return getAnnotationBy(annotationClass);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	private <A> A getAnnotationBy(Class<A> annotationClass)	throws ClassNotFoundException, NoSuchClassError {
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		ClassPool classPool = ClassPool.getDefault();
-		return (A) attribute.getAnnotation(annotationClass.getName()).toAnnotationType(classLoader, classPool);
-	}
-	
 	public String getName() {
 		return methodInfo.getName();
 	}
 	
-	public boolean isAnnotationPresent(Class<? extends Annotation> annotationClass) {
-		if (attribute == null) return false;
-		
-		return null != attribute.getAnnotation(annotationClass.getName());
-	}
 	
 }
