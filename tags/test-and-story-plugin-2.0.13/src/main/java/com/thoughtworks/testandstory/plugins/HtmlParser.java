f73ca598f6ac1ee1b40a777811e6e958e54cc67b
yan925@gmail.com <yan925@gmail.com@c7dc0373-66dd-5fc0-b770-3954006a1860>
2012-11-14 08:21:00 +0000

    copy for tag test-and-story-plugin-2.0.13

---
+++ tags/test-and-story-plugin-2.0.13/src/main/java/com/thoughtworks/testandstory/plugins/HtmlParser.java
@@ -0,0 +1,46 @@
+package com.thoughtworks.testandstory.plugins;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class HtmlParser
+{
+
+    public String getStorySummary(String storyUrl) {
+        BufferedReader jiraPage = null;
+        try {
+            jiraPage = new BufferedReader(new InputStreamReader(new URL(storyUrl).openStream()));
+            return getStorySummary(jiraPage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (jiraPage != null) {
+                try {
+                    jiraPage.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return "Not Found";
+    }
+    
+    public String getStorySummary(BufferedReader jiraPage) throws IOException {
+        String currentLine = jiraPage.readLine();
+        while (currentLine != null) {
+            if (currentLine.trim().startsWith("<title>")) {
+                return parse(currentLine);
+            }
+            currentLine = jiraPage.readLine();
+        }
+        return "Not Found";
+    }
+
+    // <title>[#LSP-473] to capture the oldest policy holder&#39;s DoB - Suncorp Central Jira </title>
+    private String parse(String currentLine)
+    {
+        return currentLine.replace(" - Suncorp Central Jira </title>", "").substring(currentLine.indexOf(']') + 1);
+    }
+}
---
+++ tags/test-and-story-plugin-2.0.13/src/test/java/com/thoughtworks/testandstory/plugins/TestIncludeThridPartyPackage.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.testandstory.plugins;
+
+import org.junit.Test;
+
+public class TestIncludeThridPartyPackage {
+	
+	@Test
+	@Story("8413")
+	public void test1() {
+		
+	}
+
+}
